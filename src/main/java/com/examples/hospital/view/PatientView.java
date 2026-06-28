1af1739ede609655de47234f5badd8326ef95e1a
kanchanparkash <bajajkanchan84@gmail.com>
2026-06-29 00:28:15 +0200

    implemented Patient controller operations

---
+++ src/main/java/com/examples/hospital/view/PatientView.java
@@ -0,0 +1,11 @@
+package com.examples.hospital.view;
+
+import java.util.List;
+
+import com.examples.hospital.model.Patient;
+
+public interface PatientView {
+
+	void showAllPatients(List<Patient> patients);
+
+}
