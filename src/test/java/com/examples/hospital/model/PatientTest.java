019e3c4f60427ffe75a1c27b796bef0296a4e7a1
kanchanparkash <bajajkanchan84@gmail.com>
2026-06-28 16:16:31 +0200

    refactor: clean up Patient tests

--- src/test/java/com/examples/hospital/model/PatientTest.java
+++ src/test/java/com/examples/hospital/model/PatientTest.java
@@ -7,28 +7,6 @@ import org.junit.Test;
 public class PatientTest {
 
 	@Test
-	public void testEmptyConstructorAndSetters() {
-		Patient patient = new Patient();
-		patient.setId("1");
-		patient.setName("Marco");
-
-		assertThat(patient.getId())
-			.isEqualTo("1");
-		assertThat(patient.getName())
-			.isEqualTo("Marco");
-	}
-
-	@Test
-	public void testConstructorAndGetters() {
-		Patient patient = new Patient("1", "Marco");
-
-		assertThat(patient.getId())
-			.isEqualTo("1");
-		assertThat(patient.getName())
-			.isEqualTo("Marco");
-	}
-
-	@Test
 	public void testEquals() {
 		Patient patient = new Patient("1", "Marco");
 		Patient samePatient = new Patient("1", "Marco");
