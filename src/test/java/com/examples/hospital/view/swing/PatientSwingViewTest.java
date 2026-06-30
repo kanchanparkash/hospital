d1d086d701b77cc6fba95b16977644461c551339
kanchanparkash <bajajkanchan84@gmail.com>
2026-06-30 14:33:12 +0200

    Red: enable delete button when patient is selected

--- src/test/java/com/examples/hospital/view/swing/PatientSwingViewTest.java
+++ src/test/java/com/examples/hospital/view/swing/PatientSwingViewTest.java
@@ -5,10 +5,13 @@ import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.core.matcher.JLabelMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JButtonFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.examples.hospital.model.Patient;
 
 @RunWith(GUITestRunner.class)
 public class PatientSwingViewTest extends AssertJSwingJUnitTestCase {
@@ -44,5 +47,17 @@ public class PatientSwingViewTest extends AssertJSwingJUnitTestCase {
 		window.textBox("idTextBox").enterText("1");
 		window.textBox("nameTextBox").enterText("Giuseppe Bianchi");
 		window.button(JButtonMatcher.withText("Add")).requireEnabled();
+	}
+
+	@Test
+	public void testDeleteButtonShouldBeEnabledOnlyWhenAPatientIsSelected() {
+		GuiActionRunner.execute(
+			() -> patientSwingView.getListPatientsModel().addElement(new Patient("1", "Giuseppe Bianchi"))
+		);
+		window.list("patientList").selectItem(0);
+		JButtonFixture deleteButton = window.button(JButtonMatcher.withText("Delete Selected"));
+		deleteButton.requireEnabled();
+		window.list("patientList").clearSelection();
+		deleteButton.requireDisabled();
 	}
 }
