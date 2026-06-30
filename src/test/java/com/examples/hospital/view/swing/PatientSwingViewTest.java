bfc21075192173818e4ca117eafe765937169e41
kanchanparkash <bajajkanchan84@gmail.com>
2026-06-30 14:14:16 +0200

    Red: added failing patient swing view initial test

---
+++ src/test/java/com/examples/hospital/view/swing/PatientSwingViewTest.java
@@ -0,0 +1,41 @@
+package com.examples.hospital.view.swing;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.core.matcher.JLabelMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GUITestRunner.class)
+public class PatientSwingViewTest extends AssertJSwingJUnitTestCase {
+
+	private FrameFixture window;
+
+	private PatientSwingView patientSwingView;
+
+	@Override
+	protected void onSetUp() {
+		GuiActionRunner.execute(() -> {
+			patientSwingView = new PatientSwingView();
+			return patientSwingView;
+		});
+		window = new FrameFixture(robot(), patientSwingView);
+		window.show();
+	}
+
+	@Test @GUITest
+	public void testControlsInitialStates() {
+		window.label(JLabelMatcher.withText("id"));
+		window.textBox("idTextBox").requireEnabled();
+		window.label(JLabelMatcher.withText("name"));
+		window.textBox("nameTextBox").requireEnabled();
+		window.button(JButtonMatcher.withText("Add")).requireDisabled();
+		window.list("patientList");
+		window.button(JButtonMatcher.withText("Delete Selected")).requireDisabled();
+		window.label("errorMessageLabel").requireText(" ");
+	}
+}
